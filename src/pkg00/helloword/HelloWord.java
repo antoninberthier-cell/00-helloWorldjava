dc87e74deba42a2e41550ce42891c8cfb5aa246c
aberthier2 <aberthier2@350028R-F115X03.lise.kreizenn.bzh>
2026-09-22 17:33:45 +0200

    Exercice 2

--- src/pkg00/helloword/HelloWord.java
+++ src/pkg00/helloword/HelloWord.java
@@ -2,20 +2,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pkg00.helloword;
 
+package pkg00.helloword;
+import java.util.Scanner;
+
+
+        
 /**
  *
  * @author aberthier2
  */
 public class HelloWord {
-
- 
-     */
+  
     public static void main(String[] args) {
-        
+       /*Exo2 
        System.out.println("bonjour");
-       System.out.println("Salut");
+       System.out.println("Salut");*/
+      Scanner sc = new Scanner(System.in);
+      int x = 0;
+      int y = 0;
+      int z =0;
+      
+      System.out.print("Saisir la valeur de x: ");
+      
+      x = sc.nextInt();
+              
+      System.out.print("Saisir la valeur de y: ");
+      
+      y = sc.nextInt();
+      
+      System.out.println("Avant permutation : x= " + x + "  y= " + y);
+     
+      
+      z = y;
+      y = x;
+      x = z;
+      System.out.println("Apres permutation : x= " + z + "  y= " + y);
+
+      
+      
+    
+      
+ 
+
+       
 
     }
     
